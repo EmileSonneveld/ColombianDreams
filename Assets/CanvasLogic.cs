fe10d5cbb2e4a95abe45049bab51320748b8ff2b
Emile Sonneveld <emile.sonneveld@chili-publish.com>
2016-12-03 10:29:42 +0100

    menue functionality tweaking

--- Assets/CanvasLogic.cs
+++ Assets/CanvasLogic.cs
@@ -7,6 +7,7 @@ public class CanvasLogic : MonoBehaviour
 
 	public GameObject informationPanel;
 	public GameObject destinationInfoPanel;
+	public GameObject mainPanel;
 
 	public List<Vector2> planeMapImagePos = new List<Vector2>();
 	void Start()
@@ -22,21 +23,25 @@ public class CanvasLogic : MonoBehaviour
 	public void FlightInfoButton_Clicked()
 	{
 		informationPanel.SetActive(true);
+		mainPanel.SetActive(false);
 	}
 
 	public void FlightInfoCloseButton_Clicked()
 	{
 		informationPanel.SetActive(false);
+		mainPanel.SetActive(true);
 	}
 
 	public void DestinationInfoButton_Cliked()
 	{
 		destinationInfoPanel.SetActive(true);
+		mainPanel.SetActive(false);
 	}
 
 	public void InformationPanelCloseButton_Click()
 	{
 		destinationInfoPanel.SetActive(false);
+		mainPanel.SetActive(true);
 	}
 
 }
