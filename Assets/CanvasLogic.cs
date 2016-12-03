5990aca1e4b1c2db5e6a0e37be26778a681ec49e
Emile Sonneveld <emile.sonneveld@chili-publish.com>
2016-12-03 02:50:47 +0100

    dl htfjdf

---
+++ Assets/CanvasLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasLogic : MonoBehaviour
+{
+
+	public GameObject informationPanel;
+	public GameObject destinationInfoPanel;
+
+	public List<Vector2> planeMapImagePos = new List<Vector2>();
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void FlightInfoButton_Clicked()
+	{
+		informationPanel.SetActive(true);
+	}
+
+	public void FlightInfoCloseButton_Clicked()
+	{
+		informationPanel.SetActive(false);
+	}
+
+	public void DestinationInfoButton_Cliked()
+	{
+		destinationInfoPanel.SetActive(true);
+	}
+
+	public void InformationPanelCloseButton_Click()
+	{
+		destinationInfoPanel.SetActive(false);
+	}
+
+}
